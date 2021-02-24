68db086fb0cf42c73438f52e4f7219d5b49c5dda
guilhermemd <guilherme.md@gmail.com>
2021-02-24 15:22:26 -0300

    termina parte II mas com duvidas

--- exercises/bloco_04/4.1/parte-II-script.js
+++ exercises/bloco_04/4.1/parte-II-script.js
@@ -1,5 +1,7 @@
 const name = "Guilherme Dornelles";
 const birthCity = "Porto Alegre";
-var birthYear = 1987;
+var birthYear = 2020;
 
+console.log(name);
+console.log(birthCity);
 console.log(birthYear);
