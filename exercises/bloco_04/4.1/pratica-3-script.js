17fe0673891425f0e1c0613ab26355ec17cf0537
guilhermemd <guilherme.md@gmail.com>
2021-02-25 00:07:03 -0300

    adiciona pratica-3

---
+++ exercises/bloco_04/4.1/pratica-3-script.js
@@ -0,0 +1,13 @@
+var a = 1;
+var b = 2;
+var c = 3;
+
+if (a > b && a > c){
+  console.log('A var a tem o maior valor');
+}
+else if (b > a && b > c){
+  console.log('A var b tem o maior valor')
+}
+else {
+  console.log('A var c tem o maior valor')
+}
