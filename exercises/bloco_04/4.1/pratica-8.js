e918a490f8a798a4226151b6240df4398f1148da
guilhermemd <guilherme.md@gmail.com>
2021-02-27 19:00:34 -0300

    adiona pratica 8 mais o bonus

--- exercises/bloco_04/4.1/pratica-8.js
+++ exercises/bloco_04/4.1/pratica-8.js
@@ -3,6 +3,7 @@
 var a = 5;
 var b = 4;
 var c = 3;
+var resultado;
 
 if (a % 2 ===0 || b % 2 === 0 || c % 2 === 0){
   resultado = true;
@@ -11,4 +12,17 @@ else {
   resultado = false;
 }
 
+console.log(resultado);
+
+// Bonus: use somente um if . 
+
+var a = 5;
+var b = 4;
+var c = 3;
+
+var resultado = false;
+
+if (a % 2 ===0 || b % 2 === 0 || c % 2 === 0){
+  resultado = true;
+}
 console.log(resultado);
