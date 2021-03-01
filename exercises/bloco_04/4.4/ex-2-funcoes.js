45e4d03f0433b67a32aafdb394a7b9dd3b8c0038
guilhermemd <guilherme.md@gmail.com>
2021-03-01 18:20:46 -0300

    corrige ex-2-funcoes colocando o console.log fora da funcao

--- exercises/bloco_04/4.4/ex-2-funcoes.js
+++ exercises/bloco_04/4.4/ex-2-funcoes.js
@@ -1,12 +1,12 @@
 // 2 - Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
 
 function whichIsBigger(a, b) {
-  if (a > b){
-    return console.log('A var a é maior que a var b');
+  if (a > b) {
+    return 'A var a é maior que a var b';
   }
   else {  
-    return console.log('A var b é maior que a var a');
+    return 'A var b é maior que a var a';
   };
 }
 
-whichIsBigger(7, 4);
+console.log(whichIsBigger(7, 4));
