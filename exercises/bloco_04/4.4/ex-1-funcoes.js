2172720252c72e0e1f36bfe2402f84cbccd0d8ab
guilhermemd <guilherme.md@gmail.com>
2021-03-01 16:11:07 -0300

    adiciona 1-exercicio de funcoes

---
+++ exercises/bloco_04/4.4/ex-1-funcoes.js
@@ -0,0 +1,37 @@
+/* 
+Adição
+Subtração
+Multiplicação
+Divisão
+Módulo 
+*/
+
+function sum(a, b){
+  return a + b;
+}
+
+console.log(sum(5, 6));
+
+function sub(a, b){
+  return a - b;
+}
+
+console.log(sub(6, 2));
+
+function mult(a, b) {
+  return a * b;
+}
+
+console.log(mult(3, 2));
+
+function division(a, b) {
+  return a / b;
+}
+
+console.log(division(10, 2));
+
+function modulo(a, b) {
+  return a % b;
+}
+
+console.log(modulo(50, 5));
