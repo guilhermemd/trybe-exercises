e9f979056d2f0c9c318c6380e7e5ab93a1e9a5f3
guilhermemd <guilherme.md@gmail.com>
2021-03-02 13:55:22 -0300

    adiciona 2-funcoes resolvida

--- exercises/bloco_04/4.4/pratica-II-2-funcoes.js
+++ exercises/bloco_04/4.4/pratica-II-2-funcoes.js
@@ -3,3 +3,13 @@ Array de teste: [2, 3, 6, 7, 10, 1]; .
 Valor esperado no retorno da função: 4 .
 */
 
+function whichIsBigger(numbers){
+  var bigger = 0;
+  for (let key in numbers) {
+    if (bigger < numbers[key]){
+      bigger = numbers[key];
+    }
+  }
+  return bigger; // Guilherme do futuro. Repare que o retur tem de estar for dos laços para poder terminar as voltas das key ;;) 
+}
+console.log(whichIsBigger([2, 3, 6, 7, 10, 1]));
