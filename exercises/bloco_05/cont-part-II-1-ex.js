4e9b286bc392796c974371ff98e7432404a1a647
guilhermemd <guilherme.md@gmail.com>
2021-03-03 18:30:17 -0300

    adiciona ultima passagem

--- exercises/bloco_05/cont-part-II-1-ex.js
+++ exercises/bloco_05/cont-part-II-1-ex.js
@@ -1,3 +1,4 @@
 document.getElementById('page-title').innerHTML = 'Olá, mundo!';
 document.getElementById('second-paragraph').innerHTML = 'I dont really know what to write over here.';
 document.getElementById('subtitle').innerHTML = 'Outro Título';
+document.getElementsByClassName('teste').style.color = 'black';
