958d3716a8c9c10caea5172e85c9f05b53f68915
guilhermemd <guilherme.md@gmail.com>
2021-03-03 15:27:07 -0300

    altera subtitle por js DOM

--- exercises/bloco_05/cont-part-II-1-ex.js
+++ exercises/bloco_05/cont-part-II-1-ex.js
@@ -1,2 +1,3 @@
 document.getElementById('page-title').innerHTML = 'Olá, mundo!';
 document.getElementById('second-paragraph').innerHTML = 'I dont really know what to write over here.';
+document.getElementById('subtitle').innerHTML = 'Outro Título';
