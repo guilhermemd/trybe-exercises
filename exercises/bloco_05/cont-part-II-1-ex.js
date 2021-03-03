1829c481f6a24493c4deb2d2c066552fa8ea0f6e
guilhermemd <guilherme.md@gmail.com>
2021-03-03 15:24:58 -0300

    altera segundo apragravo por js DOM

--- exercises/bloco_05/cont-part-II-1-ex.js
+++ exercises/bloco_05/cont-part-II-1-ex.js
@@ -1,1 +1,2 @@
-document.getElementById('page-title').innerHTML = 'Olá, mundo!'
+document.getElementById('page-title').innerHTML = 'Olá, mundo!';
+document.getElementById('second-paragraph').innerHTML = 'I dont really know what to write over here.';
