99a78de778c3d2b552540939f3cb756a4c0bd2df
guilhermemd <guilherme.md@gmail.com>
2021-03-08 13:56:41 -0300

    adiciona exercicio 2 do bloco 5.3 incompleto

--- exercises/bloco_05/5.3/script.js
+++ exercises/bloco_05/5.3/script.js
@@ -14,3 +14,26 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+// Exercício 1: Mês dezembro com seus feriados
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function decemberDays() {
+  for(let index of dezDaysList) {
+    let getPosition = document.getElementById('days');
+    let creatingLi = document.createElement('li');
+    let day = dezDaysList[index];
+    if(day === 24 || day === 31){
+      getPosition.className = 'day holiday';
+      creatingLi.innerHTML = day;
+      getPosition.appendChild(createLi);
+    }
+  }
+}
+
+// Exercício 2: Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
+function btnHoliday {
+  let getLocalBtn = document.querySelector('.buttons-container');
+  let crearBtnHoliday = document.createElement('button');
+  
+}
