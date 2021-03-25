293b9c0760afcdf3152e19e6287a36e3eddc7abe
guilhermemd <guilherme.md@gmail.com>
2021-03-25 13:35:33 -0300

    part 2 exe 6

--- exercises/bloco_07/7.2/part-2.js
+++ exercises/bloco_07/7.2/part-2.js
@@ -19,22 +19,38 @@ const lesson3 = {
 };
 
 //Exercicio - 1: Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela. 
-// const addNewKey = (obj, key, value) => {
-//   obj[key] = value;
-// };
+const addNewKey = (obj, key, value) => {
+  obj[key] = value;
+};
 
-// addNewKey(lesson2, 'turno', 'manhã');
+addNewKey(lesson2, 'turno', 'manhã');
 
-// console.log(lesson2);
+console.log(lesson2);
 
 //Exercício - 2: Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
-// const listKeys = (obj) =>  Object.keys(obj);
-// console.log(listKeys(lesson3));
+const listKeys = (obj) =>  Object.keys(obj);
+console.log(listKeys(lesson3));
 
 //Exercício - 3: Crie uma função para mostrar o tamanho de um objeto.
-// const objLength = (obj) =>  Object.keys(obj).length;
-// console.log(objLength(lesson3));
+const objLength = (obj) =>  Object.keys(obj).length;
+console.log(objLength(lesson3));
 
-//Exercício - 4:
+//Exercício - 4: Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 const objValue = (obj) =>  Object.values(obj);
 console.log(objValue(lesson2));
+
+//Exercício - 5: Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+const allLessons = Object.assign({},{lesson1, lesson1, lesson3 });
+console.log(allLessons)
+
+//Exercício - 6: Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+
+const getNumberOfStudents = (obj) => {
+  let total = 0;
+  const array = Object.keys(obj);
+  for (index in array) {
+    total += obj[array[index]].numeroEstudantes;
+  }
+  return total;
+};
+console.log(getNumberOfStudents(allLessons));
