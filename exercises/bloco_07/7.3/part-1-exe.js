d931b6a165c278396ee6edc7ac27d32109967384
guilhermemd <guilherme.md@gmail.com>
2021-04-01 18:39:45 -0300

    exercio bla blab

--- exercises/bloco_07/7.3/part-1-exe.js
+++ exercises/bloco_07/7.3/part-1-exe.js
@@ -20,3 +20,8 @@ function sum(a, b) {
 // 4 - exercício: Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)
 assert.throw(sum(4, '5'), 'Deveria lançar um Error');
 
+// 5 - exercício: Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")
+assert.throw(sum(4, '5'), 'Deveria lançar um Error');
+// assert.throws(() => {
+//   sum(4, '5');
+// });
