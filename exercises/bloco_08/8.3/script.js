3a8bb66b3d7ce43ab88b557b309df4629ac8039e
guilhermemd <guilherme.md@gmail.com>
2021-04-06 12:43:23 -0300

    exercicio 2 8.2

--- exercises/bloco_08/8.3/script.js
+++ exercises/bloco_08/8.3/script.js
@@ -78,11 +78,37 @@ const books = [
 // };
 // console.log(formatedBookNames()),
 
-// 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+// assert.deepStrictEqual(formatedBookNames(), expectedResult);
 
+// 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author , com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+const expectedResult = [
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];
 function nameAndAge() {
-  return books
-  .map((item) => (
+  return books.map((item) => (
     {
     author: item.author.name,
     age: item.releaseYear - item.author.birthYear,
@@ -92,4 +118,5 @@ function nameAndAge() {
 
 console.log(nameAndAge());
 
-assert.deepStrictEqual(formatedBookNames(), expectedResult);
+assert.deepStrictEqual(nameAndAge(), expectedResult);
+
