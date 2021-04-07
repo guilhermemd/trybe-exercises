dfd302174401ee244e757256a9b4e1e052df0d64
guilhermemd <guilherme.md@gmail.com>
2021-04-07 18:37:28 -0300

    exercicio 2 - 8.5

--- exercises/bloco_08/8.5/script.js
+++ exercises/bloco_08/8.5/script.js
@@ -1,13 +1,25 @@
-// 1 - Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. O código deve retornar em sequência 2 , 15 e 54 .
+// // 1 - Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. O código deve retornar em sequência 2 , 15 e 54 .
+// const assert = require('assert');
+// // original - const rectangleArea = (width, height) => width * height;
+// const rectangleArea = (width, height) => width * height;
+
+// const rectangle1 = [1, 2];
+// const rectangle2 = [3, 5];
+// const rectangle3 = [6, 9];
+// const rectangles = [rectangle1, rectangle2, rectangle3];
+
+// rectangles.forEach((rectangle) => {
+//   assert.strictEqual(rectangleArea(...rectangle), rectangle[0] * rectangle[1]) // altere a chamada da funcao rectangleArea
+// });
+
+// 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos. Ou seja:
+
 const assert = require('assert');
-// original - const rectangleArea = (width, height) => width * height;
-const rectangleArea = (width, height) => width * height;
 
-const rectangle1 = [1, 2];
-const rectangle2 = [3, 5];
-const rectangle3 = [6, 9];
-const rectangles = [rectangle1, rectangle2, rectangle3];
+const sum = (...spread) => spread.reduce((acc, item) => acc + item, 0);
 
-rectangles.forEach((rectangle) => {
-  assert.strictEqual(rectangleArea(...rectangle), rectangle[0] * rectangle[1]) // altere a chamada da funcao rectangleArea
-});
+assert.strictEqual(sum(), 0);
+assert.strictEqual(sum(1), 1);
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(sum(1, 2, 3), 6);
+assert.strictEqual(sum(1, 2, 3, 4), 10);
