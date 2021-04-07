4d6308ca08ae572cfe5129c920e21456da0b7a9e
guilhermemd <guilherme.md@gmail.com>
2021-04-07 18:31:37 -0300

    exercicio 1 - 8.5

--- exercises/bloco_08/8.5/script.js
+++ exercises/bloco_08/8.5/script.js
@@ -1,6 +1,6 @@
 // 1 - Dado o código abaixo, complete-o de forma que seja impressa a área dos 3 retângulos. O código deve retornar em sequência 2 , 15 e 54 .
 const assert = require('assert');
-
+// original - const rectangleArea = (width, height) => width * height;
 const rectangleArea = (width, height) => width * height;
 
 const rectangle1 = [1, 2];
@@ -9,5 +9,5 @@ const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
 
 rectangles.forEach((rectangle) => {
-  assert.strictEqual(rectangleArea(), rectangle[0] * rectangle[1]) // altere a chamada da funcao rectangleArea
+  assert.strictEqual(rectangleArea(...rectangle), rectangle[0] * rectangle[1]) // altere a chamada da funcao rectangleArea
 });
