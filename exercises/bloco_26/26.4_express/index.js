e28f5858768a001659a269fa3f4d4da6759c8aeb
guilhermemd <guilherme.md@gmail.com>
2021-08-27 20:24:06 -0300

    feat: :sparkles: exercio 4

--- exercises/bloco_26/26.4_express/index.js
+++ exercises/bloco_26/26.4_express/index.js
@@ -6,6 +6,14 @@
 // Sua rota deve receber, no body da requisição, o seguinte JSON: { "name": "<nome do usuário>" }
 // Sua rota deve retornar o seguinte JSON: { "message": "Hello, <nome do usuário>!" } .
 
+// 3 - Crie uma rota POST /greetings
+// Sua rota deve receber o seguinte JSON: { "name": "<nome do usuário>", "age": <idade do usuário> } .
+// Caso a pessoa usuária tenha idade superior a 17 anos, devolva o JSON { "message": "Hello, <nome do usuário>!" } com o status code 200 - OK .
+// Caso a pessoa usuária tenha 17 anos ou menos, devolva o JSON { "message": "Unauthorized" } com o status code 401 - Unauthorized .
+
+// 4 - Crie uma rota PUT /users/:name/:age .
+// Sua rota deve retornar o seguinte JSON: { "message": "Seu nome é <name> e você tem <age> anos de idade" } .
+
 // const express = require('express');
 // const app = express();
 
@@ -15,18 +23,43 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-app.get('/ping', handleHelloWorldRequest); 
+//exercicio 1
+app.get('/ping', (_req, res) => 
+  res.status(200).json( { message: 'pong' } ) 
+) ;
 
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
-}); // 3
-
-function handleHelloWorldRequest(_req, res) {
-  res.status(200).json( { message: 'pong' } ); 
-}
+});
 
 // exercicio 2
 app.post('/hello', function (req, res) {
-  const { nomeUsuario } = req.body;
-  res.status(200).json( { message: `Hello, ${nomeUsuario}` })
+  const { name } = req.body;
+  res.status(200).json( { message: `Hello, ${name}` })
+});
+
+// Exercio 3
+app.post('/greetings', function (req, res) {
+  const { name, age } = req.body;
+  if(age < 18) return res.status(401).json({ message: "Unauthorized" })
+  res.status(200).json( { message: `Hello, ${name}` })
+});
+
+// Exercicio 4
+
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+  res.status(200).json( { message: `Seu nome é ${name} e você tem ${age} anos de idade` })
+})
+
+app.put('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
+
+  res.status(204).end();
 });
