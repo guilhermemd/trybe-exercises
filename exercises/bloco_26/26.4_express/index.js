38fc2f24d72e336c70a3ab7b9b7a71059b82396f
guilhermemd <guilherme.md@gmail.com>
2021-08-27 20:25:38 -0300

    feat: :sparkles: exercicio 4

--- exercises/bloco_26/26.4_express/index.js
+++ exercises/bloco_26/26.4_express/index.js
@@ -51,15 +51,3 @@ app.put('/users/:name/:age', (req, res) => {
   const { name, age } = req.params;
   res.status(200).json( { message: `Seu nome é ${name} e você tem ${age} anos de idade` })
 })
-
-app.put('/recipes/:id', function (req, res) {
-  const { id } = req.params;
-  const { name, price } = req.body;
-  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
-
-  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
-
-  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
-
-  res.status(204).end();
-});
